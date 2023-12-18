7fca9f27a863c1e20d7189240ddc4e1eba34cbea
Rick South <rick@natamus.com>
2023-12-18 10:17:58 +0100

    (v3.4) Improved mod icon compression, which lowers the jar file size. [+2]

--- Common/src/main/java/com/natamus/moveminecarts/util/Reference.java
+++ Common/src/main/java/com/natamus/moveminecarts/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.moveminecarts.util;
 public class Reference {
 	public static final String MOD_ID = "moveminecarts";
 	public static final String NAME = "Move Minecarts";
-	public static final String VERSION = "3.3";
+	public static final String VERSION = "3.4";
 	public static final String ACCEPTED_VERSIONS = "[1.19.2]";
 }
