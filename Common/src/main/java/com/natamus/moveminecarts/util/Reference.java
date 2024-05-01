36b6187fc2b5b1a2fc34153e7b852c40ae07cd65
Rick South <rick@natamus.com>
2024-05-01 16:35:58 +0200

    Move Minecarts source code for 1.20.6.

---
+++ Common/src/main/java/com/natamus/moveminecarts/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.moveminecarts.util;
+
+public class Reference {
+	public static final String MOD_ID = "moveminecarts";
+	public static final String NAME = "Move Minecarts";
+	public static final String VERSION = "3.5";
+	public static final String ACCEPTED_VERSIONS = "[1.20.6]";
+}
