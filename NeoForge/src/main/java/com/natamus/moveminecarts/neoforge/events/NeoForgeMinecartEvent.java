9078b3eb2d83a0dc3847e2fb66a78d1a1c15b839
Rick South <rick@natamus.com>
2025-02-12 23:24:58 +0100

    (v3.7) (Forge & Neo) Removed redundant event register annotations. [+1]

--- NeoForge/src/main/java/com/natamus/moveminecarts/neoforge/events/NeoForgeMinecartEvent.java
+++ NeoForge/src/main/java/com/natamus/moveminecarts/neoforge/events/NeoForgeMinecartEvent.java
@@ -7,11 +7,9 @@ import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
 import net.neoforged.neoforge.event.tick.PlayerTickEvent;
 
-@EventBusSubscriber
 public class NeoForgeMinecartEvent {
 	@SubscribeEvent
 	public static void onPlayerTick(PlayerTickEvent.Pre e) {
