1ea3e24703a8f0a9229fc9e2f348d74a4562b6de
Rick South <rick@natamus.com>
2025-02-12 23:24:54 +0100

    (v3.7) (Forge & Neo) Removed redundant event register annotations. [+1]

--- Forge/src/main/java/com/natamus/moveminecarts/ModForge.java
+++ Forge/src/main/java/com/natamus/moveminecarts/ModForge.java
@@ -28,7 +28,7 @@ public class ModForge {
 	}
 
 	private void loadComplete(final FMLLoadCompleteEvent event) {
-    	MinecraftForge.EVENT_BUS.register(new ForgeMinecartEvent());
+    	MinecraftForge.EVENT_BUS.register(ForgeMinecartEvent.class);
 	}
 
 	private static void setGlobalConstants() {
