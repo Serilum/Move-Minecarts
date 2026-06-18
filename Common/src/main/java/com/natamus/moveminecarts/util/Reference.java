e2ac86a64c31bb205f293da2788d851eb0e03e8f
Rick South <rick@natamus.com>
2026-06-18 23:39:19 +0200

    Updated Move Minecarts to Minecraft version 26.2.0.

--- Common/src/main/java/com/natamus/moveminecarts/util/Reference.java
+++ Common/src/main/java/com/natamus/moveminecarts/util/Reference.java
@@ -4,5 +4,5 @@ public class Reference {
 	public static final String MOD_ID = "moveminecarts";
 	public static final String NAME = "Move Minecarts";
 	public static final String VERSION = "3.8";
-	public static final String ACCEPTED_VERSIONS = "[26.1.2]";
+	public static final String ACCEPTED_VERSIONS = "[26.2.0]";
 }
